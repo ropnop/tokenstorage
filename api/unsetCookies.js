b5e41c06d8c7b2458095676ae063697f6ffabeab
Ronnie Flathers <rflathers@marqeta.com>
2020-10-03 17:51:50 -0500

    change response

--- api/unsetCookies.js
+++ api/unsetCookies.js
@@ -1,11 +1,12 @@
 const cookie = require('cookie');
 
 module.exports = async (req, res) => {
+    cookieToken = req.cookies['token'];
     res.setHeader('Set-Cookie', cookie.serialize("token", '', {
         maxAge: -1,
         path: "/",
         sameSite: true,
         httpOnly: true,
     }))
-    res.send('ok');
+    res.send(`Cookie ${cookieToken} unset!`);
 }
