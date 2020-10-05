3c48b20aee390e9b39372abdd2d904583d5b483e
Ronnie Flathers <rflathers@marqeta.com>
2020-10-04 19:12:30 -0500

    commented code

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,13 +1,4 @@
-const whitelistedOrigins = [
-    "http://localhost", // dev
-    "http://localhost:3000", // dev
-    "https://tokenstorage.ropnop.dev", // prod
-]
-
-const whitelistedPathRegex = /\/api\/[^.]*$/ // anything under /api
-
-let token = '';
-
+// These listeners will make the service worker immediately available for the page
 self.addEventListener('install', function(event) {
     event.waitUntil(self.skipWaiting());
     console.log('[SW] serviceworker installed!');
@@ -18,6 +9,20 @@ self.addEventListener('activate', function(event) {
     console.log('[SW] serviceworker ready!');
 });
 
+// Hardocded checks for origins/paths to send credentials to
+const whitelistedOrigins = [
+    "http://localhost", // dev
+    "http://localhost:3000", // dev
+    "https://tokenstorage.ropnop.dev", // prod
+]
+
+const whitelistedPathRegex = /\/api\/[^.]*$/ // anything under /api
+
+// Global token variable in the service worker
+let token = '';
+
+
+// Exposed "method" for saving the token
 self.addEventListener('message', function(event) {
     if (event.data && event.data.type === 'SET_TOKEN') {
         token = event.data.token;
@@ -25,7 +30,7 @@ self.addEventListener('message', function(event) {
     }
 })
 
-
+// Helper function to add the auth header if the oubound request matches the whitelists
 const addAuthHeader = function (event) {
     destURL = new URL(event.request.url);
     if (whitelistedOrigins.includes(destURL.origin) && whitelistedPathRegex.test(destURL.pathname)) {
@@ -38,6 +43,7 @@ const addAuthHeader = function (event) {
     }
 }
 
+// Intercept all fetch requests and add the auth header
 self.addEventListener('fetch', addAuthHeader);
 
 
